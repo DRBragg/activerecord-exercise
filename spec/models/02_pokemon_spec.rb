d4ecff382a5643069cf375f828bac920ea45d146
Drew Bragg <DRBragg@gmail.com>
2017-08-24 20:19:58 -0400

    fixed spelling mistake

--- spec/models/02_pokemon_spec.rb
+++ spec/models/02_pokemon_spec.rb
@@ -2,10 +2,10 @@ require 'spec_helper'
 
 describe Pokemon, type: :model do
   it { should have_valid(:name).when("Pikachu", "Squirtle", "Jigglypuff") }
-  it { should_not have_vaild(:name).when('', nil)}
+  it { should_not have_valid(:name).when('', nil)}
 
   it { should have_valid(:level).when(10, 99, 27) }
-  it { should_not have_vaild(:level).when('1', nil, 101, -1)}
+  it { should_not have_valid(:level).when('1', nil, 101, -1)}
 
   it { should have_many(:types).through(:monster_types) }
   it { should belong_to :trainer }
--- spec/models/03_type_spec.rb
+++ spec/models/03_type_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Type, type: :model do
   it { should have_valid(:name).when("Electric", "Rock", "Ghost") }
-  it { should_not have_vaild(:name).when('', nil)}
+  it { should_not have_valid(:name).when('', nil)}
 
   it { should have_many(:pokemons).through(:monster_types) }
   it { should have_many(:pokemons).through(:monster_types) }
