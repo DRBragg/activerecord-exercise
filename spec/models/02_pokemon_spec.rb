80a86e774e1fe22d891b8d2630a26f188abfe72d
Drew Bragg <DRBragg@gmail.com>
2017-08-24 22:04:19 -0400

    fixed grammatical errors

--- spec/models/02_pokemon_spec.rb
+++ spec/models/02_pokemon_spec.rb
@@ -5,7 +5,7 @@ describe Pokemon, type: :model do
   it { should_not have_valid(:name).when('', nil)}
 
   it { should have_valid(:level).when(10, 99, 27) }
-  it { should_not have_valid(:level).when('1', nil, 101, -1)}
+  it { should_not have_valid(:level).when('#1', nil, 101, -1)}
 
   it { should have_many(:types).through(:monster_types) }
   it { should belong_to :trainer }
